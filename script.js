1217b8e98c62993925a113cf3ea04c510998a83a
Heenrike <henriqueex250@gmail.com>
2023-05-18 18:15:03 -0300

    style

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 let botao = document.querySelector(".adicionar");
 let apagar = document.querySelector(".apagar");
 let redefinir = document.querySelector(".redefinir");
+let tipo = document.querySelector(".tipo");
 
 let numero = document.querySelector(".numeros");
-let body = document.querySelector("body");
 let meuBotao = document.getElementById("meuBotao");
-meuBotao.addEventListener("click", function() {
-let meuInput = document.getElementById("meuInput");
-  let soma = meuInput.value;
-  // Faça algo com o valor do input aqui, como enviá-lo para um servidor
-console.log(soma)
- 
+let meuInput;
+
+meuBotao.addEventListener("click", function () {
+  meuInput = document.getElementById("meuInput");
+  let soma = parseInt(meuInput.value);
+  console.log(soma);
 });
 
 let number1 = 0;
 
 function soma() {
-  let cal = parseInt(meuInput.value)
+  let cal = parseInt(meuInput.value);
   let num = parseInt(numero.innerText);
-  let number1 = num + cal;
-  numero.innerText = number1;
-  botao.innerText = "Adicionar"+ " + " +cal
-
+  let result = num + cal;
+  numero.innerText = result;
+  botao.innerText = "Adicionar" + " + " + cal;
+  verificarParOuImpar(result); // Chama a função para verificar se o número é par ou ímpar
 }
 
 function zerar() {
   numero.innerText = number1;
 }
-function apaga (){
+
+function apaga() {
   let num = parseInt(numero.innerText);
-  if (num >0){ let number1 = num - 1;
-  numero.innerText = number1;}
-} 
+  if (num > 0) {
+    let result = num - 1;
+    numero.innerText = result;
+  }
+}
 
 botao.addEventListener("click", soma);
 redefinir.addEventListener("click", zerar);
-apagar.addEventListener("click", apaga)
-
-
+apagar.addEventListener("click", apaga);
